eb688e2b0bf4c15d2321abe67b132b1451f514c0
zzbiozzlx132 <trangiakhiemm@gmail.com>
2025-05-31 18:19:54 +0700

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', 
+  base: '/', // Chính xác cho custom domain
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
